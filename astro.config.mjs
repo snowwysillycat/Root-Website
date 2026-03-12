83d09548f3b16e949e0473bc7cd9132f70ea849b
Snowwysillycat <snowwysillycat@gmail.com>
2026-03-12 17:38:25 +0200

    fixing pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://snowwysillycat.github.io',
+    site: 'https://droot.vercel.app/',
+    base: '',
     output: 'static',
 });
